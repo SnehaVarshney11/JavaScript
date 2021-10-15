dae5e6e9460d47a2d75afefd5e2d3a53b8ddda77
SNEHA VARSHNEY <sneha.varshney_cs19@gla.ac.in>
2021-10-15 21:47:08 +0530

    Firebase Created

---
+++ 10].Firebase/crud.js
@@ -0,0 +1,73 @@
+var rollV, nameV, genderV, addressV;
+
+function readFom() {
+    rollV = document.getElementById("roll").value;
+    nameV = document.getElementById("name").value;
+    genderV = document.getElementById("gender").value;
+    addressV = document.getElementById("address").value;
+    console.log(rollV, nameV, genderV, addressV);
+}
+document.getElementById("insert").onclick = function() {
+    readFom();
+
+    firebase
+        .database() 
+        .ref("student/" + rollV)
+        .set({
+            rollNo: rollV,
+            name: nameV,
+            gender: genderV,
+            address: addressV,
+        });
+        alert("Data Inserted");
+        document.getElementById("roll").value = "";
+        document.getElementById("name").value = "";
+        document.getElementById("gender").value = "";
+        document.getElementById("address").value = "";
+};
+
+document.getElementById("read").onclick = function() {
+    readFom();
+
+    firebase
+        .database() 
+        .ref("student/" + rollV)
+        .on("value", function (snap){
+            document.getElementById("roll").value = snap.val().rollNo;
+            document.getElementById("name").value = snap.val().name;
+            document.getElementById("gender").value = snap.val().gender;
+            document.getElementById("address").value = snap.val().address;
+        });
+};
+
+document.getElementById("update").onclick = function() {
+    readFom();
+
+    firebase
+        .database() 
+        .ref("student/" + rollV)
+        .update({
+            name: nameV,
+            gender: genderV,
+            address: addressV,
+        });
+        alert("Data Updated");
+        document.getElementById("roll").value = "";
+        document.getElementById("name").value = "";
+        document.getElementById("gender").value = "";
+        document.getElementById("address").value = "";
+};
+
+document.getElementById("delete").onclick = function() {
+    readFom();
+
+    firebase
+        .database() 
+        .ref("student/" + rollV)
+        .remove();
+        alert("Data Deleted");
+        document.getElementById("roll").value = "";
+        document.getElementById("name").value = "";
+        document.getElementById("gender").value = "";
+        document.getElementById("address").value = "";
+};
---
+++ 10].Firebase/fireConfig.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyCvocdJv9yuOwe8oPQOvzz2NI0lPyB7ZoA",
+  authDomain: "myfireprowithcrud-ce88c.firebaseapp.com",
+  databaseURL: "https://myfireprowithcrud-ce88c-default-rtdb.firebaseio.com",
+  projectId: "myfireprowithcrud-ce88c",
+  storageBucket: "myfireprowithcrud-ce88c.appspot.com",
+  messagingSenderId: "742744931102",
+  appId: "1:742744931102:web:d5a39063e91fd9cb46270f"
+};
+
+//Initialize Firebase
+firebase.initializeApp(firebaseConfig);
