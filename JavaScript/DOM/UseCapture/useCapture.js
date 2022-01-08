1ba935b047fe930e473fa7369b9bf6c875c397ed
Sneha Varshney <sneha.varshney_cs19@gla.ac.in>
2022-01-08 14:25:25 +0530

    useCapture js

---
+++ JavaScript/DOM/UseCapture/useCapture.js
@@ -0,0 +1,8 @@
+document.querySelector("#inner").addEventListener('click', function(){
+    alert('Inner Div');
+},true);
+
+document.querySelector("#outer").addEventListener('click', function(){
+    alert('Outer Div');
+},true);
+
